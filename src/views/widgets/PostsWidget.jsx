224a690898a93a85594b36b90f8bd2a7e47e785b
Yoana <yoanabarbie@icloud.com>
2024-08-09 18:57:52 +0200

    fix: fixed import and export errors in home + widgets

--- src/views/widgets/PostsWidget.jsx
+++ src/views/widgets/PostsWidget.jsx
@@ -35,39 +35,39 @@ const PostsWidget = ({ userId, isProfile = false }) => {
     } else {
       getPosts();
     }
-  }, []);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <>
-        {posts.map(
-            ({
-                _id,
-                userId,
-                firstName,
-                lastName,
-                description,
-                location,
-                picturePath,
-                userPicturePath,
-                likes,
-                comments
-            }) => (
-                <PostWidget
-                key={_id}
-                postId={_id}
-                postUserId={userId}
-                name={`${firstName} ${lastName}`}
-                description={description}
-                location={location}
-                picturePath={picturePath}
-                userPicturePath={userPicturePath}
-                likes={likes}
-                comments={comments}>
-                </PostWidget>
-            )
-        )}
+      {posts.map(
+        ({
+          _id,
+          userId,
+          firstName,
+          lastName,
+          description,
+          location,
+          picturePath,
+          userPicturePath,
+          likes,
+          comments,
+        }) => (
+          <PostWidget
+            key={_id}
+            postId={_id}
+            postUserId={userId}
+            name={`${firstName} ${lastName}`}
+            description={description}
+            location={location}
+            picturePath={picturePath}
+            userPicturePath={userPicturePath}
+            likes={likes}
+            comments={comments}
+          />
+        )
+      )}
     </>
-  )
-}
+  );
+};
 
 export default PostsWidget;
